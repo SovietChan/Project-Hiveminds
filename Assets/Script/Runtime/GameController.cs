4c44d1f37b67d72e03848634a9f2314c4a7f6413
Bintang Fathur Rahman <bintangfr99@gmail.com>
2023-02-04 15:21:47 +0700

    feat : character controller

--- Assets/Script/Runtime/GameController.cs
+++ Assets/Script/Runtime/GameController.cs
@@ -1,21 +1,17 @@
-using System.Collections;
 using System.Collections.Generic;
 using Script.Runtime.Pawn;
 using UnityEngine;
-using UnityEngine.Serialization;
 
-public class GameController : MonoBehaviour
+namespace Script.Runtime
 {
-    [FormerlySerializedAs("_characterControllerl")] [SerializeField] private PlayerController playerControllerl;
-    // Start is called before the first frame update
-    void Start()
+    public class GameController : MonoBehaviour
     {
+        [SerializeField] private List<PlayerController> characterList;
+        void Start()
+        {
         
-    }
+        }
 
-    private void Init()
-    {
         
     }
-    
 }
--- Assets/Script/Runtime/Pawn/PlayerController.cs
+++ Assets/Script/Runtime/Pawn/PlayerController.cs
@@ -4,42 +4,77 @@ namespace Script.Runtime.Pawn
 {
     public class PlayerController : MonoBehaviour
     {
-        private CharacterController controller;
-        private Vector3 playerVelocity;
-        private bool groundedPlayer;
-        private float playerSpeed = 3.0f;
-        private float jumpHeight = 2.0f;
-        private float gravityValue = -9.81f;
+        private Vector3 _playerVelocity;
+        private bool _groundedPlayer;
+        [SerializeField] private float playerSpeed = 3.0f;
+        [SerializeField] private float jumpHeight = 2.0f;
+        [SerializeField] private float gravityValue = -9.81f;
 
-        private void Start()
+        [SerializeField]
+        private bool _isControlled;
+        [SerializeField]
+        private bool _isInfected;
+        [SerializeField]
+        private CharacterType _type;
+        
+
+        private CharacterController GetCharacterController()
         {
-            controller = gameObject.GetComponent<CharacterController>();
+            return gameObject.GetComponent<CharacterController>();
         }
 
         private void Update()
         {
-            groundedPlayer = controller.isGrounded;
-            if (groundedPlayer && playerVelocity.y < 0)
+            GetInput();
+        }
+
+        private void InfectChar()
+        {
+            _isInfected = true;
+        }
+
+        private void ControlChar(bool isControlled)
+        {
+            _isControlled = isControlled;
+        }
+
+        private void GetInput()
+        {
+            if (!_isControlled && !_isInfected) return;
+            _groundedPlayer = GetCharacterController().isGrounded;
+            if (_groundedPlayer && _playerVelocity.y < 0) _playerVelocity.y = 0f;
+
+            // var move = _type == CharacterType.Beetle
+            //     ? new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0)
+            //     : new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+
+            var move = new Vector3();
+            if (_type == CharacterType.Beetle && GetCharacterController().isGrounded)
             {
-                playerVelocity.y = 0f;
+                move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+            }
+            else if (GetCharacterController().isGrounded)
+            {
+                move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
             }
 
-            var move = new Vector3(Input.GetAxis("Horizontal"), 0,0 );
-            controller.Move(move * (Time.deltaTime * playerSpeed));
+            GetCharacterController().Move(move * (Time.deltaTime * playerSpeed));
 
-            if (move != Vector3.zero)
-            {
-                gameObject.transform.position = move;
-            }
+            if (move != Vector3.zero) gameObject.transform.position = move;
 
             // Changes the height position of the player..
-            if (Input.GetButtonDown("Jump") && groundedPlayer)
-            {
-                playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-            }
+            if (Input.GetButtonDown("Jump") && _groundedPlayer && _type == CharacterType.GrassHopper)
+                _playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
 
-            playerVelocity.y += gravityValue * Time.deltaTime;
-            controller.Move(playerVelocity * Time.deltaTime);
+            _playerVelocity.y += gravityValue * Time.deltaTime;
+            GetCharacterController().Move(_playerVelocity * Time.deltaTime);
         }
+    }
+
+    public enum CharacterType
+    {
+        Ant,
+        GrassHopper,
+        Beetle
     }
 }
