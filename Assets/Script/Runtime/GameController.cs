149bf88ecacc10eca7ff256041f7d0b50113067c
Bintang Fathur Rahman <bintangfr99@gmail.com>
2023-02-04 13:06:57 +0700

    feat: character movement

--- Assets/Script/Runtime/GameController.cs
+++ Assets/Script/Runtime/GameController.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Script.Runtime.Pawn;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class GameController : MonoBehaviour
 {
+    [FormerlySerializedAs("_characterControllerl")] [SerializeField] private PlayerController playerControllerl;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Init()
     {
         
     }
+    
 }
